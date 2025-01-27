a72ac098ce435144934657be6d7ff0a1fe62016a
Rahil-Huseynov <rahil.huseynov.05@gmail.com>
2025-01-27 13:52:57 +0400

    Login template for azmiu library update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,4 @@
 import logo from '../assets/8f061feb6f9876d9bc40e7ef1a42970a.webp';
-// import devide from '../assets/line.png'
 import './Home.css';
 
 const Home = () => {
@@ -9,9 +8,7 @@ const Home = () => {
                 <div className="left_section">
                     <img src={logo} alt="logo" className="arrow_icon" />
                     <p className="university_name">Azərbaycan Memarlıq və İnşaat Universiteti</p>
-                    <div>
-                        <p className='sisteme_giris_item'>Sİstemə gİrİş</p>
-                    </div>
+                    <p className='sisteme_giris_item'>Sİstemə gİrİş</p>
                     <div className='input_container'>
                         <input type="text" placeholder="İstifadəçi adı" className="input_field" />
                         <input type="password" placeholder="Şifrə" className="input_field" />
